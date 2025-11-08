c1d22275eb9f92a1592acdfc0bbe3dc8d473ba03
Massimo Gramcko <m.gramcko@correo.unimet.edu.ve>
2025-11-08 13:59:36 -0400

    Class Main ajustes

--- src/proyecto_sv/Main.java
+++ src/proyecto_sv/Main.java
@@ -14,7 +14,31 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        // --- 1. EL CÓDIGO "NIMBUS" ---
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception ex) {
+            java.util.logging.Logger.getLogger(VentanaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        // --- FIN DEL CÓDIGO NIMBUS ---
+
+        
+        // --- 2. EL CÓDIGO PARA ARRANCAR TU VENTANA ---
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                
+                // --- ¡ESTA ES LA LÍNEA CORREGIDA! ---
+                // Le pasamos el modo por defecto (ADMINISTRADOR)
+                // para que coincida con tu constructor.
+                new VentanaPrincipal(ModoUsuario.ADMINISTRADOR).setVisible(true);
+            }
+        });
     }
     
 }
